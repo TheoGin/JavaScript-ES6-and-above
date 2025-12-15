3f751e75d9f21da7e9bbfbf28f157ffe313b30cf
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-15 23:00:07 +0800

    feat: 11.1 实现状态的变化（after listen）

--- 11. 手写Promise/11.1 实现状态的变化/MyPromise.js
+++ 11. 手写Promise/11.1 实现状态的变化/MyPromise.js
@@ -1,72 +1,69 @@
-const PromiseStatus = {
-  PENDING: "pending",
-  FULFILLED: "fulfilled",
-  REJECTED: "rejected",
-};
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
 
 class MyPromise {
 
-  // 私有
-  #status = "";
-  #data;
-  #reason;
-
+  /**
+   * 立即执行函数
+   * @param{ Function } executor
+   */
   constructor(executor) {
-    this.#status = PromiseStatus.PENDING;
-    console.log(this.#status);
-    executor(this.resolve.bind(this), this.reject.bind(this));
-  }
-
-  resolve(data) {
-    console.log(this);
-    this.#data = data
-    if (this.#status === PromiseStatus.PENDING) {
-      this.#status = PromiseStatus.FULFILLED;
-    }
-    console.log(this.#status);
-    return this;
-  }
-
-  reject(reason) {
-    if (this.#status === PromiseStatus.PENDING) {
-      this.#status = PromiseStatus.REJECTED;
-    }
-    // console.log(reason);
-    this.#reason = reason
-    console.log(this.#status);
-    return this;
-  }
-
-  then(onFulfilled) {
-    onFulfilled(this.#data)
-    return this;
-  }
-
-  catch(onRejected) {
+    this._state = PENDING;
+    this._value = undefined;
     try {
-      onRejected(this.#reason)
-    } catch (err) {
-
+      executor(this.resolve.bind(this), this.reject.bind(this)); // 可能会报错
+    } catch (e) {
+      this.reject(e);
     }
-    return this;
+  }
+
+  /**
+   * 修改状态和值
+   * @param{ PENDING | FULFILLED | REJECTED } state
+   * @param { any } value
+   */
+  changeState(state, value) {
+    // 状态不可逆
+    if (this._state !== PENDING) {
+      return;
+    }
+    this._state = state;
+    this._value = value;
+  }
+
+  /**
+   * 成功状态的任务
+   * @param { any } data
+   */
+  resolve(data) {
+    // 修改状态和值
+    this.changeState(FULFILLED, data);
+  }
+
+  /**
+   * 拒绝状态的任务
+   * @param { any } reason
+   */
+  reject(reason) {
+    // 修改状态和值
+    this.changeState(FULFILLED, reason);
   }
 
 }
 
-const promise = new Promise((resolve, reject) => {
-  reject(new Error('err'));
+/*const promise = new Promise((resolve, reject) => {
+  reject(new Error("err"));
 }).then((data) => {
   console.log(data);
 }).catch((reason) => {
   console.log(reason);
-});
+});*/
 
 const myPromise = new MyPromise((resolve, reject) => {
-  // resolve(1);
-  reject(new Error('err'));
-}).then((data) => {
-  console.log(data);
-}).catch(reason => {
-  console.log(reason);
-})
+  resolve(1);
+  reject(new Error("err"));
+  // throw new Error("err");
+});
+console.log(myPromise);
 
