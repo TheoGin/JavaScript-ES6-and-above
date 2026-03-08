d6ec60a96c6c70f1e94c07e2a971500f0cd27d62
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-08 22:18:43 +0800

    fix: 7.3 知名（公共、具名）符号

--- ES6Particular/7. 符号/7.3 知名（公共、具名）符号/7.3.3 [扩展] Symbol.toPrimitive.js
+++ ES6Particular/7. 符号/7.3 知名（公共、具名）符号/7.3.3 [扩展] Symbol.toPrimitive.js
@@ -12,10 +12,11 @@ class Temperature {
     this.degree = degree;
   }
 
+  // 该知名符号会影响类型转换的结果
   [Symbol.toPrimitive](type) {
     switch (type) {
       case "default":
-        return this.degree + "摄氏度！";
+        return this.degree + "摄氏度";
       case "number":
         return this.degree;
       case "string":
@@ -25,7 +26,13 @@ class Temperature {
 }
 
 const temperature = new Temperature(30);
-console.log(temperature + 123); // 30摄氏度！123
+
+// 1. type: default
+console.log(temperature + '！'); // 30摄氏度！
+
+// 2. type: number
 console.log(temperature / 2); // 15
+
+// 3. type: string
 console.log(String(temperature)); // 30℃
 
