c6a90dd9d8685b3d9716cb40ccc1d705fca39afd
傅朝俊 <fuchaojun@szkingdom.com>
2026-04-03 22:31:59 +0800

    feat: 11.3 [扩展]手写set

---
+++ ES6Particular/11. 更多的集合类型/11.3 [扩展]手写set/MySet.js
@@ -0,0 +1,69 @@
+class MySet {
+  _data = [];
+
+  constructor(iterable) {
+    if (iterable === undefined) {
+      this._data = [];
+      this.size = 0;
+      return
+    }
+
+    if (typeof iterable[Symbol.iterator] !== "function") {
+      throw new TypeError(`${ typeof iterable } ${ iterable } is not iterable (cannot read property Symbol(Symbol.iterator))`);
+    }
+
+    for (const item of iterable) {
+      this.add(item);
+    }
+  }
+
+  add(value) {
+    for (const item of this._data) {
+      if (!this.has(value)) {
+        this._data.push(value);
+      }
+    }
+
+    return this;
+  }
+
+  has(value) {
+    for (const item of this._data) {
+      if (this.isEqual(item, value)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isEqual(value1, value2) {
+    // 严格相等 不区分 +0 和 -0
+    if (value1 === 0 && value2 === 0) {
+      return true;
+    }
+
+    return value1 === value2;
+  }
+
+  delete(value) {
+    for (let i = 0; i < this._data.length; i++) {
+      if (this.isEqual(this._data[i], value)) {
+        this._data.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  forEach(callback) {
+    for (const item of this._data) {
+      callback(item, item, this);
+    }
+  }
+
+  * [Symbol.iterator]() {
+    for (const item of this._data) {
+      yield item;
+    }
+  }
+}
